c531701a7e393cc0aa7ddc16053c72d92eb8417f
Maikon Sposito <mknsposito@gmail.com>
2026-04-04 12:47:58 -0300

    refactor: add @AuthenticationPrincipal in infra web controller

--- src/main/kotlin/br/com/maikonspo/consumo/infra/delivery/web/WaterExpensePageController.kt
+++ src/main/kotlin/br/com/maikonspo/consumo/infra/delivery/web/WaterExpensePageController.kt
@@ -2,6 +2,7 @@ package br.com.maikonspo.consumo.infra.delivery.web
 
 import br.com.maikonspo.consumo.core.usecase.FindWaterExpensesByUserUseCase
 import br.com.maikonspo.consumo.infra.security.UserDetailsImpl
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -13,11 +14,12 @@ class WaterExpensePageController(
 ) {
 
     @GetMapping("/water-expenses")
-    fun listPage(model: Model): String {
-        val authentication = SecurityContextHolder.getContext().authentication
-        val userDetails = authentication!!.principal as UserDetailsImpl
-        val userId = userDetails.getUser().id!!
+    fun listPage(
+        model: Model,
+        @AuthenticationPrincipal userDetails: UserDetailsImpl
+    ): String {
 
+        val userId = userDetails.getUser().id!!
         val expenses = findWaterExpensesByUserUseCase.execute(userId)
 
         model.addAttribute("expenses", expenses)
@@ -25,12 +27,14 @@ class WaterExpensePageController(
     }
 
     @GetMapping("/water-expenses/form")
-    fun formPage(model: Model): String {
-        val authentication = SecurityContextHolder.getContext().authentication
-        val userDetails = authentication!!.principal as UserDetailsImpl
-        val userId = userDetails.getUser().id!!
+    fun formPage(
+        model: Model,
+        @AuthenticationPrincipal userDetails: UserDetailsImpl
+    ): String {
 
+        val userId = userDetails.getUser().id!!
         model.addAttribute("userId", userId)
+
         return "water-expense-form"
     }
 }
